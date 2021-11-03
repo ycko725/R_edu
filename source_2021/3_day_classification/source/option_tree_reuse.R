48fb97ecb288a80d7ac8342e8848f8b005d351fd
dschloe <j2hoon85@gmail.com>
2021-11-04 01:01:35 +0900

    updated

---
+++ source_2021/3_day_classification/source/option_tree_reuse.R
@@ -0,0 +1,55 @@
+# ---- 의사결정나무의 용어 ---- 
+# 뿌리(Root Node): Tree 구조가 시작되는 노드(모든 데이터로 구성됨)
+# 끝마디 잎 (Terminal Node): Tree 구조의 끝에 위치하는 노드, Terminal 노드 이후, 더 이상 분기는 없음
+# 부모 노드(Parent Node) / 자식 노드(Child Node): 분기 되기 전 마디를 부모노드, 분기 된 후 노드들을 자식노드라고 함
+# 중간마디(Internal Node): Tree 구조의 중간에 위치하는 마디
+# 가지(Branch): 뿌리에서 끝마디까지 연결된 하나의 연결형태를 의미
+# 깊이(Depth): 가지를 이루는 Node의 단계수를 의미
+
+# ---- 분기기준(Splitting Criterion) ----
+# 분기기준이란 부모노드에서 자식노드로 분기될 때, 어떠한 독립변수를 이용하여 어떻게 범주를 구분하여 설정하는 것이 종속변수 분포를 잘 구분할 수 있는지를 결정하는 기준
+# 이 때, 종속변수 분포를 구별하는 정도를 순수도, 비순수도라는 지표를 산정
+
+# ---- 분리기준에 사용하는 지표 ----
+# 카이제곱 통계량의 p값: 분석대상 자료의 검정통계량 x2의 유의확률 p값이 가장 작은 독립변수와 이에 따른 최적 분리로 Child Node 구성
+# 지니지수(Gini Index): 데이터의 비순수도를 나타내는 지니지수를 측정하여 이를 감소시켜 주는 독립변수와 이에 따른 최적분리로 Child Node로 구성됨
+# 엔트로피 지수(Entropy Index): 비순수도를 나타내는 지표로서 이를 감소시켜주는 독립변수와 이에 따른 최적분리로 Child 노드를 구성함
+# ANOVA Table의 F 통계량 p값: ANOVA Table의 F 통계량에 대한 유의확률 p값이 가장 작은 독립변수와 이에 따른 최적 분리로 Child 노드를 구성
+# 분산의 감소량(Variance Reduction): 종속변수의 실측치와 모형에 의한 추정치와의 오차를 최소화하는 기준과 같이, 분산을 최소화하는 독립변수와 이에 따른 최적분리로 Child Node 구성
+
+library(rpart)
+library(rpart.plot)
+library(ggplot2)
+
+data(iris)
+str(iris)
+
+ggplot(iris, aes(x = Petal.Length, y = Petal.Width, colour = Species)) + 
+  geom_point(size = 2) + 
+  theme_minimal()
+
+# ---- 모형 생성 ---- 
+train <- sample(1:150, 100)
+tree <- rpart(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
+              , data = iris
+              , subset = train
+              , method = "class")
+
+# 학습된 내용 확인
+summary(tree)
+
+# 그래프 확인
+rpart.plot(tree)
+
+# 가지치기(Prunning) 수행하기
+printcp(tree)
+ 
+# 하이퍼 파라미터
+pruned_tree = prune(tree, cp = 0.1) # 가지치기
+rpart.plot(pruned_tree)
+
+predict(pruned_tree, iris[-train, ], type = "class")
+
+
+# Decision Tree / RandomForest / LightGBM / XGBoost
+
---
+++ source_2021/8_day_kaggle_project/kaggle_credit_card/pca_practice.R
@@ -0,0 +1,70 @@
+# PCA(Principal Component Analysis) 분석
+# - 차원축소(Dimensionality Reduction)와 변수추출(Feature Extraction)기법으로 널리 쓰임. 
+# - 이론 참고자료: https://ratsgo.github.io/machine%20learning/2017/04/24/PCA/
+# - 본 장에서는 주로 코드 설명 위주로 작성함. 
+# 변수 간의 공분산/상관을 조사하는 스펙트럼 분해(Spectral Decomposition)
+# 개체 간의 공분산/상관을 조사하는 특이값 분해(Singular Value Decomposition)
+# 주로 쓰이는 함수는 마찬가지로 크게 두가지임
+# - princomp(): Spectral Decomposition
+# - prcomp(): Singular Value Decomposition
+# - R 문서에 따르면 prcomp()의 정확도가 매우 약간 좋다고 함. 
+# - 그러나 여기에서는 PCA() 함수를 주로 사용하도록 함. 
+
+# install.packages("factoextra")
+library(factoextra)
+library(FactoMineR)
+library(tidyverse)
+?prcomp
+# prcomp(x, scale = FALSE)
+# - x: numeric matrix or data frame
+# - scale: 데이터 스케일링 여부
+
+# ---- 데이터 수집 ---- 
+?decathlon2
+data("decathlon2")
+glimpse(decathlon2)
+
+# 마지막 3개의 데이터는 수치 데이터와 거리가 멀다. 
+new_data = decathlon2[1:23, 1:10]
+glimpse(new_data)
+
+# Data Standardization
+# -- Scaling: 각 수치형의 범위가 다 다름
+# 예): g vs kg, m vs km 동일하게 사용가능한가? 
+# 각각의 데이터의 수치를 그대로 비교가 가능한가? 
+# 식 (x1 - mean(x)) / sd(x))
+# --> x1은 임의의 한개의 데이터
+# --> mean(x)은 전체 데이터의 평균
+# --> sd은 전체데이터의 표준편차
+
+# ---- PCA 모형 적합 ----
+# PCA(X, scale.unit = TRUE, ncp = 5, graph = TRUE)
+# - X: 데이터 프레임, 행과 열은 모두 수치형 데이터로 구성이 되어야 함
+# - scale.unit = TRUE, TRUE값을 지정하면 스케일링을 함
+# - ncp: 최종 결괏값의 차원의 개수를 지정할 수 있다. 
+# - graph: A logical value. 
+
+iris_pca_m = PCA(iris[, -5], graph = FALSE) # TRUE
+iris_pca_m
+
+# PCA의 수학적 이해
+# get_eigenvalue(res.pca): eigenvalues(고유값)/Variance(분산)
+# -- 분산이 가장 큰 방향(또는)이 데이터에서 가장 많은 정도를 담고 있는 방향이다. 
+# -- 데이터의 분산이 클수록 데이터를 의미있게 분석할 수 있다. 
+
+eig_val = get_eigenvalue(iris_pca_m)
+eig_val
+
+# 해석
+# - eigenvalue 모든 합을 구하면 
+# 2.91849782 + 0.91403047 + 0.14675688 + 0.02071484 / [1] 4
+# variance.percent : 2.91849782 / 4
+# 만약에 eigenvalue > 1, 각 PC는 표준화된 데이터의 원래 변수 중 하나에서 설명하는 것보다 더 많은 분산을 차지한다는 것을 나타냄. 이는 일반적으로 PC가 유지되는 컷오프 포인트로 사용됩니다. 이는 데이터가 표준화되었을 때만 유효함. 
+# PCA (Kaiser 1961)
+fviz_eig(iris_pca_m, addlabels = TRUE, ylim = c(0, 80))
+# 약 2 dimension이면 충분히 많은 정보를 담고 있다고 생각할 수 있음. 
+
+var <- get_pca_var(iris_pca_m)
+var$contrib
+var$cos2
+var$coord
