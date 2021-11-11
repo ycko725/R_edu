e5124db624576827e33bbf8aa8d764902f079a51
dschloe <j2hoon85@gmail.com>
2021-11-11 11:31:39 +0900

    updated

--- source_2021/7_day_textmining/ch03_2_kaggle_sentiment_analysis.R
+++ source_2021/7_day_textmining/ch03_2_kaggle_sentiment_analysis.R
@@ -46,19 +46,23 @@ ggplot(data.frame(prop.table(table(raw_reviews$Dept))), aes(x=Var1, y = Freq*100
 
 # ---- 감성 사전 데이터 셋 변환 ---- 
 glimpse(raw_reviews)
+
+idx = sample(1:nrow(raw_reviews), nrow(raw_reviews) * 0.2, replace = FALSE)
+raw_reviews = raw_reviews[idx, ]
+
 raw_reviews %>% 
   mutate(pos_binary = ifelse(Liked > 0, 1, 0)) %>% # 이산형 변수로 변환
   select(Liked, pos_binary) -> pos_binary_df
 
-idx = sample(1:nrow(raw_reviews), nrow(raw_reviews) * 0.1, replace = FALSE)
-pos_df = pos_binary_df[idx, ]
+pos_binary_df$pos_binary <- as.factor(pos_binary_df$pos_binary)
 
 # 0과 1 비교
-summary(as.factor(pos_df$pos_binary))
+summary(as.factor(pos_binary_df$pos_binary))
 
 # ---- 키워드 점수 계산 위한 데이터셋 생성
 # 텍스트 데이터 추출
-REVIEW_TEXT = as.character(reviews$Review)
+REVIEW_TEXT = as.character(raw_reviews$Review)
+REVIEW_TEXT = tolower(REVIEW_TEXT)
 
 TEXT_Token = c()
 for(i in 1:length(REVIEW_TEXT)) {
@@ -79,8 +83,45 @@ Corpus_tm_token = tm_map(Corpus_token, removePunctuation)
 Corpus_tm_token = tm_map(Corpus_token, removeNumbers)
 Corpus_tm_token = tm_map(Corpus_token, removeWords, c(stopwords("english")))
 
-TDM_Token = TermDocumentMatrix(Corpus_tm_token)
-TDM_Matrix_Token = as.matrix(TDM_Token)
+DTM_Token = DocumentTermMatrix(Corpus_tm_token)
+DTM_Matrix_Token = as.matrix(DTM_Token)
 
 # 상위 키워드 추출
-# - quantile() 함수를 
+# - quantile() 함수 활용
+top_1_pnt_words = colSums(DTM_Matrix_Token) > quantile(colSums(DTM_Matrix_Token), probs = 0.99)
+
+DTM_Matrix_Token_selected = DTM_Matrix_Token[, top_1_pnt_words]
+ncol(DTM_Matrix_Token_selected)
+
+DTM_df = as.data.frame(DTM_Matrix_Token_selected)
+pos_final_df = cbind(pos_binary_df, DTM_df)
+
+pos_final_df
+
+# ---- 훈련 검증용 데이터 분류
+set.seed(1234)
+idx = sample(1:nrow(pos_final_df), nrow(pos_df) * 0.7, replace = FALSE)
+train = pos_final_df[idx, ]
+test = pos_final_df[-idx, ]
+
+# ---- 변수 선택법에 의한 모형 개발 ----
+glimpse(train)
+
+# 로지스틱 회귀 모형 개발 시간 측정
+Start_Time = Sys.time()
+
+glm_model = step(glm(pos_binary  ~ ., data = train[,-1],
+               family = binomial(link = "logit")),direction = "backward")
+
+End_Time = Sys.time()
+
+difftime(End_Time,Start_Time, unit = "secs")
+
+summary(glm_model)
+
+# ---- 모형 성능 측정 ----
+# install.packages("pROC")
+library(pROC)
+preds = predict(glm_model, newdata = test, type = 'response')
+roc_glm = roc(test$pos_binary, preds)
+plot.roc(roc_glm, print.auc = TRUE)
