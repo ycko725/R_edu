9fa0715236e5c7fd57f70ccf00c776f6aa528203
dschloe <j2hoon85@gmail.com>
2022-02-04 00:43:24 +0900

    updated

---
+++ source_2021/2_day_stat_regression/source/2_4_scales.R
@@ -0,0 +1,21 @@
+#--------------------------------#
+#### 1. 음반 판매량 데이터 수집 ####
+#--------------------------------#
+albums = read.csv("album.csv")
+str(albums)
+
+#--------------------------------#
+#### 2. scale() 함수 적용 ####
+#--------------------------------#
+z_scores_sales = scale(albums$sales)
+
+
+#--------------------------------#
+#### 3. 기본함수 시각화 ####
+#--------------------------------#
+par(mfrow = c(1, 2))
+
+hist(albums$sales, main = "Original Data", xlab = "Sales")
+
+# Plot the histogram for the Z-scores
+hist(z_scores_sales, main = "Scaled Sales", xlab = "Z-score")
